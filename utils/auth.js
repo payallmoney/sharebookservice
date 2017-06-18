46ad7da9622bfdd0907e377107847144f5ed6152
payallmoney <payallmoney@163.com>
2017-06-18 13:13:11 +0800

    1,增加了列表

--- utils/auth.js
+++ utils/auth.js
@@ -10,7 +10,7 @@ function register(params) {
     var collection = db.collection('auth_user');
     return collection.findOne({ _id: params.userid }).then((doc) => {
         if (!doc) {
-            return collection.insertOne({ _id: params.userid, password: params.password }).then(() => {
+            return collection.insertOne({ _id: params.userid, password: params.password, score: 10, sharescore: 10 }).then(() => {
                 return { success: true, msg: "注册成功!" };
             })
         } else {
@@ -33,9 +33,26 @@ mqtt.regMqttFuncs("login", (param, clientid) => {
         login(param).then((data) => {
             console.log(data);
             if (data) {
-                loginedusers[param.loginid] = clientid;
+                data.password = '******';
+                //去掉以前的登录信息
+                let lastuserid = getUserIdByClientId(clientid);
+                while (lastuserid) {
+                    delete loginedusers[lastuserid];
+                    lastuserid = getUserIdByClientId(clientid);
+                }
+                loginedusers[param.userid] = clientid;
                 mqtt.setLogined(clientid, true);
-                resolve({ success: true, code: 200, msg: "登录成功!" });
+                var point = data.sharepoint;
+                point = point ? point : 0;
+                let level = point / 1000 + 1;
+                let sharelevel = point / 1000 + 1;
+                let ret = {
+                    userid: data._id,
+                    username: data.name,
+                    level: level,
+                    sharelevel: sharelevel
+                }
+                resolve({ success: true, code: 200, msg: "登录成功!", data: ret });
             } else {
                 resolve({ success: false, code: 401, msg: "登录失败!用户名或密码错误!" });
             }
@@ -45,7 +62,7 @@ mqtt.regMqttFuncs("login", (param, clientid) => {
 mqtt.regMqttFuncs("auth/logout", (param, clientid) => {
     return new Promise((resolve, reject) => {
         //TODO 登录
-        delete loginedusers[param.loginid];
+        delete loginedusers[param.userid];
         mqtt.setLogined(clientid, false);;
         resolve(true);
     });
@@ -66,6 +83,20 @@ mqtt.regMqttFuncs("register", (param, clientid) => {
     });
 });
 
+mqtt.regMqttFuncs("auth/accountinfo", (param, clientid) => {
+    return new Promise((resolve, reject) => {
+        //TODO 登录
+        let user = getUserIdByClientId(clientid);
+        resolve({ success: true, msg: "查询成功!", data: user });
+    });
+});
+
+
+mqtt.regMqttFuncs("auth/checklogin", (param, clientid) => {
+    return new Promise((resolve, reject) => {
+        resolve({ success: true, msg: "已登录!" });
+    });
+});
 
 module.exports = {
     login: login,
--- utils/book.js
+++ utils/book.js
@@ -1,26 +1,34 @@
 let DB = require("utils/db.js");
 let mqtt = require("utils/mqtt");
-let auth = require("utils/auth")
+let auth = require("utils/auth");
+let moment = require('moment');
+let ObjectID = require('mongodb').ObjectID;
+// let zhcn = require('moment/locale/zh-cn.js');
 module.exports = {
     share: share
 };
 
-
+//
 function share(params, clientid) {
     var db = DB.getDB();
     var userid = auth.getUserIdByClientId(clientid);
+    console.log("userid==", userid);
     var collection = db.collection('books');
+    //根据时区减去16小时后记录的时间和当前时间一致
+    params.sharedate = new Date();
+
     return collection.insertOne(params).then((doc) => {
-        db.collection('auth_user').findOneAndUpdate({ _id: userid }, { $inc: { point: 100, level: 1, sharelevel: 1 } });
+        db.collection('auth_user').findOneAndUpdate({ _id: userid }, { $inc: { sharepoint: 100 } });
         return { success: true, msg: "分享成功!" };
     });
 }
+
 mqtt.regMqttFuncs("auth/share", (param, clientid) => {
     //分享
     var userid = auth.getUserIdByClientId(clientid);
     param.userid = userid;
     //const params = { userid: userid, bookinfo: param };
-    return share(param)
+    return share(param, clientid)
 });
 
 mqtt.regMqttFuncs("booklist", (param, clientid) => {
@@ -42,7 +50,124 @@ mqtt.regMqttFuncs("booklist", (param, clientid) => {
 
     });
 });
+mqtt.regMqttFuncs("auth/sharelist", (param, clientid) => {
+    return new Promise((resolve, reject) => {
+        let userid = auth.getUserIdByClientId(clientid);
+        var db = DB.getDB();
+        var collection = db.collection('books');
+        var pagesize = param.page;
+        var skip = (pagesize - 1) * 10;
+        console.log(userid);
+        collection.find({ $query: { userid: userid }, $orderby: { sharedate: -1 } }, { skip: skip, limit: 10 }).toArray(
+            (err, docs) => {
+                if (err) {
+                    reject({ success: true, msg: "查询错误!请稍后再试!", err: err });
+                } else {
+                    resolve({ success: true, msg: "查询成功!", books: docs });
+                }
+            }
+        );
 
+    });
+});
+
+mqtt.regMqttFuncs("auth/borrowhistory", (param, clientid) => {
+    return new Promise((resolve, reject) => {
+        let userid = auth.getUserIdByClientId(clientid);
+        var db = DB.getDB();
+        var collection = db.collection('borrow');
+        var pagesize = param.page;
+        var skip = (pagesize - 1) * 10;
+        console.log(userid);
+        collection.aggregate(
+            [{
+                    $match: {
+                        userid: userid
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "books",
+                        localField: "bookid",
+                        foreignField: "_id",
+                        as: "book"
+                    }
+                },
+                { $sort: { borrowtime: -1 } },
+                { $skip: skip },
+                { $limit: 10 },
+            ]).toArray(
+            (err, docs) => {
+                if (err) {
+                    reject({ success: true, msg: "查询错误!请稍后再试!", err: err });
+                } else {
+                    resolve({ success: true, msg: "查询成功!", books: docs });
+                }
+            }
+        );
+
+    });
+});
+
+
+mqtt.regMqttFuncs("top100", (param, clientid) => {
+    return new Promise((resolve, reject) => {
+        let userid = auth.getUserIdByClientId(clientid);
+        var db = DB.getDB();
+        var collection = db.collection('auth_user');
+        console.log(userid);
+        collection.find({ $query: {} }, { limit: 100 }).sort({ sharepoint: -1 })
+            .toArray(
+                (err, docs) => {
+                    if (err) {
+                        reject({ success: true, msg: "查询错误!请稍后再试!", err: err });
+                    } else {
+                        resolve({ success: true, msg: "查询成功!", top100: docs });
+                    }
+                }
+            );
+
+    });
+});
+
+
+mqtt.regMqttFuncs("auth/focuslist", (param, clientid) => {
+    return new Promise((resolve, reject) => {
+        let userid = auth.getUserIdByClientId(clientid);
+        var db = DB.getDB();
+        var collection = db.collection('focus');
+        var pagesize = param.page;
+        var skip = (pagesize - 1) * 10;
+        console.log(userid);
+        collection.aggregate(
+            [{
+                    $match: {
+                        userid: userid
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "books",
+                        localField: "bookid",
+                        foreignField: "_id",
+                        as: "book"
+                    }
+                },
+                { $sort: { borrowtime: -1 } },
+                { $skip: skip },
+                { $limit: 10 },
+            ]).toArray(
+            (err, docs) => {
+                if (err) {
+                    reject({ success: true, msg: "查询错误!请稍后再试!", err: err });
+                } else {
+                    resolve({ success: true, msg: "查询成功!", books: docs });
+                }
+            }
+        );
+
+    });
+});
 mqtt.regMqttFuncs("search", (param, clientid) => {
     var userid = auth.getUserIdByClientId(clientid);
     return new Promise((resolve, reject) => {
@@ -110,24 +235,30 @@ mqtt.regMqttFuncs("lastsearchs", (param, clientid) => {
     });
 });
 
-mqtt.regMqttFuncs("focus", (param, clientid) => {
+mqtt.regMqttFuncs("auth/focus", (param, clientid) => {
     var userid = auth.getUserIdByClientId(clientid);
     return new Promise((resolve, reject) => {
         console.log("====popularsearch=====", param);
         var db = DB.getDB();
         var collection = db.collection('focus');
-        collection.findOneAndUpdate({ userid: userid, bookid: param._id }, { $set: { focustime: new Date() } }, { upsert: true });
+        collection.findOneAndUpdate({ userid: userid, bookid: ObjectID(param._id) }, { $set: { focustime: new Date() } }, { upsert: true });
         resolve({ success: true, msg: "关注成功!" });
     });
 });
 
-mqtt.regMqttFuncs("borrow", (param, clientid) => {
+mqtt.regMqttFuncs("auth/borrow", (param, clientid) => {
     var userid = auth.getUserIdByClientId(clientid);
     return new Promise((resolve, reject) => {
         console.log("====popularsearch=====", param);
         var db = DB.getDB();
         var collection = db.collection('borrow');
-        collection.findOneAndUpdate({ userid: userid, bookid: param._id }, { $set: { focustime: new Date() } }, { upsert: true });
+        var books = db.collection('books');
+        books.findOneAndUpdate({ _id: param._id }, { $inc: { borrownum: 1 } })
+        collection.findOneAndUpdate({ userid: userid, bookid: ObjectID(param._id) }, {
+            $set: {
+                borrowtime: new Date()
+            }
+        }, { upsert: true });
         resolve({ success: true, msg: "借书成功!" });
     });
 });
